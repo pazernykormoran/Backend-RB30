4be8a6ffc289609fad6acca4fcab256bd4ff5e6b
Andrzej <andrzolide@gmail.com>
2021-12-09 10:32:17 +0100

    binance add

---
+++ RetireBefore30/Contracts/Configs/CandlesGetRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RetireBefore30.Contracts.Configs
+{
+    public class CandlesGetRequest
+    {
+        //string symbol, TimeSpan timespan, DateTime? startTime = null, DateTime? endTime = null, int? limit = null
+        [Required]
+        public string? symbol { get; set; }
+        [Required]
+        public string? period { get; set; }
+        public long startTime { get; set; } = -1;
+        public long endTime { get; set; } = -1;
+        public int? limit { get; set; } = 100;
+    }
+}
--- RetireBefore30/Controllers/ConfigController.cs
+++ RetireBefore30/Controllers/ConfigController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Binance.Net;
+using Binance.Net.Enums;
+using Microsoft.AspNetCore.Mvc;
 using RetireBefore30.Contracts.Configs;
 using RetireBefore30.Models;
 using RetireBefore30.Services;
@@ -6,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CryptoExchange.Net.ExchangeInterfaces;
 
 namespace RetireBefore30.Controllers
 {
@@ -14,11 +17,58 @@ namespace RetireBefore30.Controllers
     public class ConfigController : Controller
     {
         private readonly IConfigService _configService;
-
+        //List<IBinanceKline> lineData;
+        private IExchangeClient client;
 
         public ConfigController(IConfigService configService)
         {
             _configService = configService;
+            client = new BinanceClient();
+            
+            //iLogger.LogInformation(candles);
+        }
+
+        [HttpGet("v1/candles")]
+        public async Task<IActionResult> getCandles([FromQuery] CandlesGetRequest request)
+        {
+
+            long second = 10000000;
+            long minute = second * 60 ;
+            long period;
+            switch (request.period)
+            {
+                case "1m": period = minute; break;
+                case "3m": period = minute*3; break;
+                case "5m": period = minute*5; break;
+                case "15m": period = minute*15; break;
+                case "30m": period = minute*30; break;
+                case "1h": period = minute*60; break;
+                case "2h": period = minute*60*2; break;
+                case "4h": period = minute * 60 * 4; break;
+                case "6h": period = minute * 60 * 6; break;
+                case "8h": period = minute * 60 * 8; break;
+                case "12h": period = minute * 60 * 12; break;
+                case "1d": period = minute * 60 * 24; break;
+                case "3d": period = minute * 60 * 24 * 3; break;
+                case "1w": period = minute * 60 * 24 * 7; break;
+                case "1M": period = minute * 60 * 24 * 30; break;
+                default: period = minute * 60 * 24; break;
+            }
+            var timespan = new TimeSpan(period);
+
+            DateTime fromDate;
+            DateTime toDate;
+            if (request.startTime != -1 && request.endTime != -1)
+            {
+                fromDate = DateTimeOffset.FromUnixTimeMilliseconds(request.startTime).UtcDateTime;
+                toDate = DateTimeOffset.FromUnixTimeMilliseconds(request.endTime).UtcDateTime;
+                return Ok(await client.GetKlinesAsync(request.symbol, timespan, fromDate, toDate, limit: request.limit));
+            }
+            else
+            {
+                return Ok(await client.GetKlinesAsync(request.symbol, timespan, limit: request.limit));
+            }
+            
         }
 
         [HttpGet("v1/configs/{configId}")]
